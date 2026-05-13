da94afc03193b08e430de8afb250acbf8c676733
VICTOR HUGO LARES BRUNING <victor.1956@aluno.pr.senac.br>
2026-05-13 10:50:50 -0300

    Primeiro commit

---
+++ Atividade1.py
@@ -0,0 +1,6 @@
+curso_a_tarde = input("Tem curso à tarde? (sim/não): ").strip().lower()
+
+if curso_a_tarde == "sim":
+    print("Entao não vou.")
+else:
+    print("Entao eu vou.")
---
+++ Victor.py
@@ -0,0 +1,12 @@
+nota = int(input('Digite a nota do aluno: '))
+
+if nota >= 90:
+    print:('A - Excelente')
+elif nota >= 80:
+    print('B - Muito bom')
+elif nota >= 70:
+    print(' C - Bom')
+elif nota >= 60:
+    print('D - Regular')
+else:
+    print('F - Reprovado') 
